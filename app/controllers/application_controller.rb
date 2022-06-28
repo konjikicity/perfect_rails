4362de4d71824702fd13be94e2b39f77ae178b92
Syu <konjikicity@gmail.com>
2022-06-28 21:05:17 +0900

    feat: 共通のログイン処理を追加, hamlのエラーを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
   helper_method :logged_in?
 
   private
 
   def logged_in?
-    !!session[:user_id] #session[:user_id]に値が入っていればログインとしてtrue
+    !!current_user #session[:user_id]に値が入っていればログインとしてtrue
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
