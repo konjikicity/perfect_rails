155ff66a47964017288240dd261401be6467424d
Syu <konjikicity@gmail.com>
2022-07-02 12:04:01 +0900

    feat: イベント参加機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
   root 'welcome#index'
   get "/auth/:privider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  resources :events do
+    resources :tickets
+  end
 end
